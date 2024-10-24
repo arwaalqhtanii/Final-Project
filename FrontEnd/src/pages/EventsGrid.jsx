49d3081fd98352ef998260e2e9f5079304f16979
arwaalqhtanii <alqhtaniarwa414@gmail.com>
2024-10-24 13:26:25 +0300

    Arwa clean cods

--- FrontEnd/src/pages/EventsGrid.jsx
+++ FrontEnd/src/pages/EventsGrid.jsx
@@ -1,22 +1,4 @@
-// import EventCard from "./EventCard";
 
-// const EventsGrid = ({ events }) => {
-//     return (
-//         <div className="container mx-auto px-12 py-10">
-//             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
-//                 {events.length > 0 ? (
-//                     events.map((event) => (
-//                         <EventCard key={event.id} event={event} />
-//                     ))
-//                 ) : (
-//                     <p className="text-center text-xl">Sorry, no events match your search.</p>
-//                 )}
-//             </div>
-//         </div>
-//     );
-// };
-
-// export default EventsGrid;
 
 import EventCard from "./EventCard";
 
