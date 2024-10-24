eaafc438a2ba2f0a1db9b07379ecda1e2c1fac2d
yohejazi11 <yohejazi11@gmail.com>
2024-10-24 14:01:33 +0300

    add font and edite my ticket and css file edite and check ticket and profile and teckit manager

--- FrontEnd/src/components/MyTicket.jsx
+++ FrontEnd/src/components/MyTicket.jsx
@@ -1,65 +1,108 @@
-import React from 'react';
+import { useState,useEffect } from 'react';
 import { MdLocationPin } from "react-icons/md";
 import { CiCalendarDate } from "react-icons/ci";
 import { MdAccessTime } from "react-icons/md";
 import { FaTicketAlt } from "react-icons/fa";
 
-
 import Vector from '/Vector.png';
 
 function MyTicket(props) {
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+      // Function to handle screen resize
+      const handleResize = () => {
+        setWindowWidth(window.innerWidth);
+      };
+  
+      // Add event listener to track window resizing
+      window.addEventListener('resize', handleResize);
+  
+      // Cleanup the event listener on component unmount
+      return () => {
+        window.removeEventListener('resize', handleResize);
+      };
+    }, []);
+
     return (
-        <div className='w-[60vw] max-md:w-[80%] h-[50vh] max-md:h-[fit-content] max-md:py-[15px] flex max-md:flex-col justify-start rounded-[10px] bg-white relative'>
-        <div className='w-[60vw] max-md:w-[80%] py-[15px] flex flex-col gap-y-[0.5rem] h-[fit-content]  rounded-[10px] bg-white relative'>
-            <div className='flex max-md:  max-md:flex-col justify-start'>
-                <div className='w-[55%] max-md:w-[100%] max-md:gap-y-[1rem] h-[100%] flex flex-col justify-between gap-y-[2rem] items-start px-[15px]'>
-                    <div className='flex max-md:pt-[3rem]'>
-                        <div className='text-[#78006E] text-[2rem] font-bold '>{props.title}</div>
-                    </div>
-                    <div className='flex items-center gap-x-[1rem]'>
-                        <div className='font-bold text-[1.5rem]'>{props.location}</div>
-                        <MdLocationPin className='text-[1.5rem]'></MdLocationPin>
-                    </div>
-                    <div className='flex flex-row-reverse max-md:w-[100%] max-md:gap-x-[1rem] max-md:justify-center'>
-                        <div className='flex flex-col items-center gap-y-[0.7rem] px-[10px]'>
-                            <FaTicketAlt className='text-[1.5rem] font-bold'></FaTicketAlt>
-                            <div>{props.type}</div>
-                        </div>
-                        <div className='flex flex-col items-center gap-y-[0.7rem] px-[10px]'>
-                            <MdAccessTime className='text-[1.5rem] font-bold'></MdAccessTime>
-                            <div>{props.time}</div>
-                        </div>
-                        <div className='flex flex-col items-center gap-y-[0.7rem] px-[10px]'>
-                            <CiCalendarDate className='text-[1.5rem] text-center font-bold'></CiCalendarDate>
-                            <div>{props.date}</div>
-                        </div>
-                    </div>
-                    <img className='w-[60%] max-md:w-[100%] h-[10px]' src={Vector}></img>
-
+        <div className={` ${windowWidth > 700 ? 'ticket-big' : 'ticket-small'} w-[50vw] max-md:w-[80%] h-[40vh]  max-md:h-[fit-content] max-md:py-[15px] flex max-md:flex-col max-md:items-center justify-start rounded-[10px] relative ${!props.Available ? 'bg-gray-300 text-gray-500' : 'bg-white'}`}>
+            <div className='w-[65%] max-md:w-[100%] h-[100%] max-md:items-center flex flex-col items-start max-md:gap-y-[1.5rem] justify-evenly  p-[10px]  pl-[35px] pr-[35px]'>
+                <div className='flex'>
+                    <div className={` text-[2rem] font-bold ${!props.Available ? 'text-gray-500' : 'text-[#78006E]'}`}>{props.title}</div>
                 </div>
-                <div className='bg-green-500 absolute  max-md:h-[fit-content] py-[15px] px-[15px] max-md:text-[1.2rem] top-0 right-2 h-[20%] flex justify-center items-center text-white border-dashed border-t-[5px] custom-dashed-border  border-white'>
-                    {props.status}
+                <div className='flex items-center gap-x-[1rem]'>
+                    <div className='font-bold text-[1.5rem]'>{props.location}</div>
+                    <MdLocationPin className='text-[1.5rem]'></MdLocationPin>
                 </div>
-                <div className='w-[30%] max-md:w-[100%] h-[100%] flex justify-end'>
-
-                    <div className='flex flex-col max-md:flex-col-reverse gap-y-[5px] items-center justify-center w-[100%] h-[100%]'>
-                        <img className='max-md:w-[60%]' src='https://www.marefa.org/w/images/8/87/QRCode.png'></img>
-                        <div className='flex flex-col items-center'>
-                            <div className='text-gray-500 font-bold max-md:text-[1.5rem]'>Ticket Code</div>
-                            <div className='font-bold max-md:text-[1.2rem]'>{props.code}</div>
-                        </div>
-
+                <div className='flex flex-row-reverse gap-x-[1rem]  max-md:w-[100%] max-md:gap-x-[1rem] max-md:justify-center'>
+                    <div className='flex flex-col items-center gap-y-[0.7rem] '>
+                        <FaTicketAlt className='text-[1.5rem] font-bold'></FaTicketAlt>
+                        <div>{props.type}</div>
                     </div>
-
+                    <div className='flex flex-col items-center gap-y-[0.7rem] '>
+                        <MdAccessTime className='text-[1.5rem] font-bold'></MdAccessTime>
+                        <div>{props.time}</div>
+                    </div>
+                    <div className='flex flex-col items-center gap-y-[0.7rem] '>
+                        <CiCalendarDate className='text-[1.5rem] text-center font-bold'></CiCalendarDate>
+                        <div>{props.date}</div>
+                    </div>
                 </div>
-
+                {windowWidth > 700 ?<img className='w-[60%] max-md:w-[100%] h-[10px]' src={Vector}></img>:null}
+                
+                {props.newPrice ? <div className='h-[40px] font-bold text-[1.2rem]'>Price : {props.newPrice} SR</div> : <div className='h-[40px]'></div>}
             </div>
 
-            <div className=' flex flex-col items-center justify-start w-[15%] max-md:w-[40%] px-[15px]'>
-                <button className='px-[23px] max-md:text-[1.5rem] w-[100%] py-[5px] rounded-[5px] text-white bg-[#78006e] hover:bg-[#be008d]' onClick={props.popSellForm}>{props.process}</button>
-            </div>
+            <div className={`w-[35%] max-md:w-[100%] h-[100%] ${windowWidth > 700 ? 'border-l-[3px]' : 'border-t-[3px]'} border-dashed  border-black p-[10px] max-md:px-[12.5px] pl-[10px] relative`}>
+                <div className='w-[100%] h-[100%] flex flex-col justify-evenly items-center relative'>
+                    {/* QR Code Section */}
+                    <div className='relative flex justify-center'>
+                        {/* QR Code with conditional blur effect */}
+                        <img
+                            className={`w-[75%] ${!props.Available ? 'filter blur-[5px]' : ''}`}
+                            src='https://www.marefa.org/w/images/8/87/QRCode.png'
+                            alt="Barcode"
+                        />
+                    </div>
 
-        </div>
+                    {/* Code Section */}
+                    <div className='relative'>
+                        <div className='font-bold'>
+                            <span>Ticket code : </span>
+                            
+                            <span className={`${!props.Available ? 'filter blur-[5px]' : ''}`}>
+                                {props.code}
+                            </span>
+                        </div>
+                    </div>
+
+
+                    {props.forbuy ? (
+                        <div className='flex gap-x-[1.5rem]'>
+                            <button className='px-[10px] py-[5px] text-white rounded-[10px] bg-slate-500 text-center font-bold'>ignore</button>
+                            <button className='px-[10px] py-[5px] text-white rounded-[10px] bg-[#78006E] text-center font-bold'>accept</button>
+                        </div>
+                    ) : (
+                        <div className='flex justify-end gap-x-[1.5rem]'>
+                            {props.Available ? (
+                                <button
+                                    className='px-[10px] py-[5px] text-white rounded-[10px] bg-[#78006E] text-center font-bold'
+                                    onClick={props.popSellForm}
+                                >
+                                    Sell
+                                </button>
+                            ) : (
+                                <button
+                                    className='px-[10px] py-[5px] text-white rounded-[10px] bg-slate-500 text-center font-bold'
+                                    disabled
+                                >
+                                    Not Available
+                                </button>
+                            )}
+                        </div>
+                    )}
+                </div>
+            </div>
         </div>
     );
 }
--- FrontEnd/src/pages/Teckitmanager.jsx
+++ FrontEnd/src/pages/Teckitmanager.jsx
@@ -1,22 +1,16 @@
-import React, { useState,useEffect } from 'react'
+import React, { useState } from 'react'
 import MyTicket from '../components/MyTicket'
 import { CiSearch } from "react-icons/ci";
 import riyadhseasonboulevard from '/riyadhseasonboulevard.jfif'
 import { AiOutlineFileSearch } from "react-icons/ai";
-import { IoClose } from "react-icons/io5";
-import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
-import axios from 'axios';
-
+import Footer from '../components/Footer';
+import { IoClose } from "react-icons/io5";
+import SellTicketModal from '../components/SellTicketModal';
 function Teckitmanager() {
 
     const [sellPop, setSellPop] = useState(false);
-    const [selectedTicket, setSelectedTicket] = useState();
-    const [tickets, setTickets] = useState([]);
-    const token = localStorage.getItem('token'); 
-    const [filterStatus, setFilterStatus] = useState(null); 
-
-
+    const [selectedTicket, setSelectedTicket] = useState()
     const handleSell = (ticketCode) => {
         setSelectedTicket(ticketCode);
         setSellPop(true);
@@ -27,88 +21,24 @@ function Teckitmanager() {
         setSellPop(false);
         document.body.style.overflow = 'auto';
     };
-
-    useEffect(() => {
-        const fetchTickets = async () => {
-            try {
-                const response = await axios.get('http://localhost:8050/tickets/Usertickets', {
-                    headers: {
-                        Authorization: `Bearer ${token}`
-                    }
-                });
-    
-                // Sort tickets by purchaseDate in descending order (most recent first)
-                const sortedTickets = response.data.tickets.sort((a, b) => new Date(b.purchaseDate) - new Date(a.purchaseDate));
-    
-                setTickets(sortedTickets); // Set the sorted tickets
-            } catch (error) {
-                console.error('Error fetching tickets:', error);
-            }
-        };
-    
-        fetchTickets();
-    }, []);
-    
-    const fetchTicketsByStatus = async (status) => {
-        try {
-            const response = await axios.get(`http://localhost:8050/tickets/ticketsUserstatus/${status}`, {
-                headers: {
-                    Authorization: `Bearer ${token}`
-                }
-            });
-            setTickets(response.data.tickets);
-             
-        } catch (error) {
-            console.error('Error fetching tickets:', error);
-        }
-    };
-
-    const handleFilter = (status) => {
-        fetchTicketsByStatus(status); 
-    };
- 
     return (
-        <div className='relative'>
-            <Navbar/>
+        <div>
             {sellPop && (
-                <div className='fixed inset-0 flex items-center justify-center z-[9999]'>
-                    <div className='fixed inset-0 bg-black opacity-80'></div>
-                    <div className='flex flex-col items-center justify-evenly max-md:w-[80%] w-[40%] h-[40vh] bg-white rounded-[10px] z-[9999999] p-[20px]'>
-                        <div className='w-[100%] text-[1.5rem] font-bold flex justify-between'>
-                            <div>Sell your Ticket</div>
-                            <button onClick={handleCloseSellPopup}>
-                                <IoClose className='text-[2rem]' />
-                            </button>
-                        </div>
-                        <div className='font-bold text-[1.5rem]'>Ticket Code: {selectedTicket}</div>
-                        <div className='font-bold text-[1.2rem]'>TO</div>
-                        <div className='flex justify-center'>
-                        <input className='px-[10px] w-full max-w-[15rem] sm:max-w-[20rem] md:max-w-[25rem] h-[50px] rounded-l-[10px] border-[1px] border-[#78006E] placeholder-gray-500' placeholder="Buyer's Email" />
-                        <button className=' rounded-r-[5px] text-white font-bold text-[1.2rem] py-2 px-9 bg-[#78006e] hover:bg-[#be008d]'>
-                            Sell
-                        </button>
-                        </div>
-
-                    </div>
-                </div>
+                <SellTicketModal></SellTicketModal>
             )}
+            <Navbar />
 
-
-            <div className=''>
-
-            </div>
             <div className='w-[100%] h-[40vh] relative  shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)]'>
                 <img className='w-[100%] h-[100%]' src={riyadhseasonboulevard}></img>
                 <div className='w-[100%] h-[100%] bg-black opacity-80 absolute top-0'></div>
                 <div className='text-white font-bold text-[3rem] text-center w-[100%] absolute top-[20%]'>Manage My Tickets</div>
-                <div className='w-[60%] max-md:w-[80%] h-[30%] flex max-md:flex-col-reverse max-md:justify-center gap-y-[10px] justify-between items-center bg-white rounded-[10px] absolute bottom-0 left-[50%] translate-x-[-50%]'>
-                    <div className='w-[40%] max-md:w-[100%] pl-4 max-md:pl-0 justify-evenly flex gap-x-[1.5rem] font-bold'>
-                        <button className='px-[16px] h-[40px] rounded-[10px] bg-[] hover:text-[#78006E]' onClick={() => handleFilter(0)}>Available</button>
-                        <button className='px-[5px] h-[40px] rounded-[10px] bg-[] hover:text-[#78006E]'onClick={() => handleFilter(1)}>Not available</button>
-
+                <div className='w-[50%] max-md:w-[80%] h-[30%] flex max-md:flex-col-reverse max-md:justify-center gap-y-[10px] justify-between items-center bg-white rounded-[10px] absolute bottom-0 left-[50%] translate-x-[-50%]'>
+                    <div className='w-[40%] max-md:w-[100%] pl-4 max-md:pl-0 max-md:justify-evenly flex gap-x-[1.5rem] font-bold'>
+                        <button className=' h-[40px] rounded-[10px] bg-[] hover:text-[#78006E]'>Available</button>
+                        <button className=' h-[40px] rounded-[10px] bg-[] hover:text-[#78006E]'>Not available</button>
                     </div>
                     <div className='w-[60%] max-md:w-[100%] max-md:justify-center flex justify-end max-md:pr-0 pr-4'>
-                        <input className='w-[80%] h-[40px] rounded-l-[10px] max-md:h-[50px] border-[1px] border-[#78006E] px-[10px] focus:outline-none'  type='search'></input>
+                        <input className='w-[80%] h-[40px] rounded-l-[10px] max-md:h-[50px] focus:outline-none px-[10px] border-[1px] border-[#78006E]' type='search'></input>
                         <button className='w-[3vw] max-md:w-[10vw] max-md:h-[50px] flex justify-center items-center bg-[#78006E] h-[40px] rounded-r-lg relative' ><AiOutlineFileSearch className='text-white text-[1.5rem] absolute'></AiOutlineFileSearch></button>
                     </div>
 
@@ -116,31 +46,16 @@ function Teckitmanager() {
             </div>
 
             <div className='w-[100%] flex flex-col gap-y-[2rem] items-center justify-center py-[10vh] bg-black'>
-              
-            {tickets.map((ticket,index) => (
-                    <MyTicket
-                        key={ticket.index} 
-                        title={ticket.eventId.name}
-                        location={ticket.eventId.location}
-                        date={ticket.visitDate}
-                        time={ticket.eventId.Time}
-                        type={ticket.ticketType}
-                        code={ticket.uniqueCode}
-                        status={ticket.updateStatus}
-                        process='Sell'
-                        popSellForm={() => handleSell(ticket.code)}
-                    />
-                ))}
-                {/* <MyTicket
+                <MyTicket
                     title='WWE RAW'
                     location='RIYADH'
                     date='17 - 04'
                     time='7:30 PM'
                     type='GOLD'
                     code='265'
-                    status='Available'
-                    process='Sell'
+                    Available={true}
                     popSellForm={() => handleSell('265')}
+
                 ></MyTicket>
                 <MyTicket
                     title='WWE RAW'
@@ -148,14 +63,14 @@ function Teckitmanager() {
                     date='17 - 04'
                     time='7:30 PM'
                     type='GOLD'
-                    code='266'
-                    status='Available'
-                    process='Sell'
-                    popSellForm={() => handleSell('266')}
-                ></MyTicket> */}
+                    code='265'
+                    Available={false}
+                    popSellForm={() => handleSell('265')}
+
+                ></MyTicket>
 
             </div>
-            <Footer/>
+            <Footer />
         </div>
     )
 }
