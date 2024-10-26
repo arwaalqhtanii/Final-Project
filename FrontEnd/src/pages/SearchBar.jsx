7ce0b642ca71dc1765f700e4ad7e59a7024c3c61
arwaalqhtanii <alqhtaniarwa414@gmail.com>
2024-10-26 15:49:53 +0300

    edit part slayder

--- FrontEnd/src/pages/SearchBar.jsx
+++ FrontEnd/src/pages/SearchBar.jsx
@@ -1,14 +1,12 @@
-
 import { useState } from 'react';
 import { FaSearch } from 'react-icons/fa';
 import Typewriter from 'react-typewriter-effect';
-
 
 const SearchBar = ({ onSearch }) => {
     const [searchTerm, setSearchTerm] = useState('');
 
     const handleSearch = () => {
-        onSearch(searchTerm); 
+        onSearch(searchTerm);
     };
 
     const handleKeyDown = (e) => {
@@ -19,27 +17,21 @@ const SearchBar = ({ onSearch }) => {
 
     return (
         <div className="container px-5 mx-auto py-8 flex gap-9 flex-col md:flex-row justify-between items-center space-y-4 md:space-y-0 pt-8">
-    <h2 className="text-3xl text-center md:text-left ml-0 md:ml-8 font-bold">Current Month Events</h2>
-    <div className="flex items-center w-full md:w-3/5 lg:w-2/5 bg-white border border-gray-300 rounded-full shadow-lg p-4 transition-all duration-300 focus-within:border-purple-800 focus-within:shadow-xl">
-        <button className="text-purple-800 ml-4 transition-transform transform hover:scale-110" onClick={handleSearch}>
-            <FaSearch className="h-6 w-6" /> 
-        </button>
-        <div className="w-full max-w-md bg-transparent focus:outline-none text-gray-700 px-4 text-left">
-        <Typewriter
-        text="Search for Riyadh Season events"
-        cursorColor="#78006e"
-        textStyle={{
-            fontSize: '16px',
-            color: '#4a5568',
-        }}
-        typeSpeed={100}
-        startDelay={500}
-    />
-    </div>
-    </div>
-    </div>
-    
-        
+            <h2 className="text-3xl text-center md:text-left ml-0 md:ml-8 font-bold">Current Month Events</h2>
+            <div className="flex items-center w-full md:w-3/5 lg:w-2/5 bg-white border border-gray-300 rounded-full shadow-lg p-4 transition-all duration-300 focus-within:border-purple-800 focus-within:shadow-xl">
+                <button className="text-purple-800 ml-4 transition-transform transform hover:scale-110" onClick={handleSearch}>
+                    <FaSearch className="h-6 w-6" />
+                </button>
+                <input
+                    type="text"
+                    className="w-full bg-transparent focus:outline-none text-gray-700 px-4 text-left"
+                    placeholder="Search for Riyadh Season events"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                    onKeyDown={handleKeyDown}
+                />
+            </div>
+        </div>
     );
 };
 
--- FrontEnd/src/pages/Slider.jsx
+++ FrontEnd/src/pages/Slider.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { FaTicketAlt } from 'react-icons/fa';
+import Typewriter from 'react-typewriter-effect';
 
 const Slider = ({ searchBarRef }) => {
     const [currentIndex, setCurrentIndex] = useState(0);
@@ -61,10 +62,22 @@ const Slider = ({ searchBarRef }) => {
             </div>
 
             <div className="absolute inset-0 flex flex-col items-center justify-center text-center text-white z-10">
-                <h1 className="text-5xl mb-4">Discover the Magic of Riyadh Season</h1>
+                <h1 className="text-6xl mb-4">Discover the Magic of Riyadh Season</h1>
                 <br />
-                <p className="text-s mb-6">Experience an unforgettable journey in the heart of the capital. Book your ticket now and get ready for days filled with <br /> entertainment events and amazing shows waiting for you.</p>
+                <Typewriter
+                text="Experience an unforgettable journey in the heart of the capital. Book your ticket now and get ready for days filled with entertainment events."
+                cursorColor="#fff"
+                textStyle={{
+                    fontSize: '17px',
+                    color: '#ffff',
+                }}
+                typeSpeed={50}
+                startDelay={500}
+            />
                 <br />
+                <br />
+                <br />
+                
                 <button
                     className="px-8 py-4 flex items-center gap-2 bg-gradient-to-r from-[#9b27b0] to-[#ff4081] text-white font-semibold rounded-full shadow-lg hover:scale-105 hover:shadow-xl transition-transform duration-300 ease-in-out animate-fade-in-up delay-300"
                     onClick={handleCheckTicketClick}
