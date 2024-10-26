39aa52614cb3ccde0f61bdef6b6c002909cd754a
arwaalqhtanii <alqhtaniarwa414@gmail.com>
2024-10-26 12:29:02 +0300

    Edit error page

--- FrontEnd/src/pages/EventsGrid.jsx
+++ FrontEnd/src/pages/EventsGrid.jsx
@@ -1,8 +1,9 @@
+
 import EventCard from "./EventCard";
 
 const EventsGrid = ({ events }) => {
     return (
-        <div className="container mx-auto px-2 py-10">
+        <div className="container mx-auto px-10 py-10">
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
                 {events.length > 0 ? (
                     events.map((event) => (
