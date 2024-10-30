476cb5ada86964eecbbc794bfcc290c64e097fa2
Haneen <htsalhagbani@gmail.com>
2024-10-30 18:46:52 +0300

    haneen update frontend5

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -10,9 +10,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 const app = express();
-// app.use(cors());
-app.options('*', cors());
-
+app.use(cors());
 app.use(express.json());
 
 connectDB();
