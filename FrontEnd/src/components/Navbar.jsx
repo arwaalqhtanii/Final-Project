ac23af5ba2c0edea5a3b47ec08cc0fb5f5e0ba71
Waleed Alhrbi <waleedalhrbi2001@gmail.com>
2024-10-30 19:41:18 +0300

    notification toggle Dropdown list

--- FrontEnd/src/components/Navbar.jsx
+++ FrontEnd/src/components/Navbar.jsx
@@ -25,7 +25,7 @@ const Navbar = () => {
     };
 
     const toggleNotificationDropdown = () => {
-        setIsProfileMenuOpen(false);
+        setIsMenuOpen(false);
         setIsNotificationOpen(!isNotificationOpen);
     };
 
